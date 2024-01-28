806008cbfc66870bc352c1df4c24718324527a3f
abdulrahim Ramadan <147656627+abdulrahim-ramadan@users.noreply.github.com>
2024-01-28 18:31:38 +0100

    add admin customization

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post , Comment
 
 # Register your models here.
+class postAdmin(admin.ModelAdmin):
+    list_display=['id','author','publish_date']
+    list_filter=['publish_date','tags']
+    search_fields=['name','content']
+    
+
+admin.site.register(Post,postAdmin)
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User,related_name='post_author' , on_delete=models.SET_NULL,null=True)
+    title = models.CharField(max_length=150)
+    content = models.TextField(max_length=30000)
+    image = models.ImageField(upload_to='posts')
+    publish_date = models.DateTimeField(default=timezone.now)
+    tags = TaggableManager()
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User,related_name='comment_author', on_delete=models.SET_NULL,null=True)
+    post = models.ForeignKey(Post,related_name='comment_post',on_delete=models.CASCADE)
+    comment = models.TextField(max_length=1000)
+    create_at = models.DateTimeField(default=timezone.now)
+
+
+
